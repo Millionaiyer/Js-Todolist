6342a8e6f3f282df4b5ae6332d64433516fc8cad
Millionaiyer <surajsaiyer@gmail.com>
2023-09-11 19:27:56 +0530

    delete finished having issue with only about validation

--- src.js
+++ src.js
@@ -1,22 +1,26 @@
 const container = document.getElementById('container');
-
-let divFlag = 0;
+const title = document.getElementById('title');
+const about = document.getElementById('about');
+// error
+const errTitle = document.querySelector('#errTitle');
+const errAbout = document.querySelector('#errAbout');
+// let printDiv = '';
+// let divFlag = 0;
 let arrTitle = [];
+
 function createDiv (titleValue,aboutValue) {
     // Creat Div
     const printDiv = document.createElement('div');
-    printDiv.id += `printContent${divFlag++}`;
+    // printDiv.id += `printContent ${divFlag++}`;
+    printDiv.id += 'printContent';
     printDiv.className = 'print'
     // printDiv.innerHTML=printDiv.id
     section.append(printDiv);
-
-    // Create Delete Button
-
-    const img = document.createElement('img');
-    img.id = '';
-    img.className = 'delete__img';
-    img.src = './asset/delete.svg'
-    printDiv.append(img);
+    
+    const printContent = document.createElement('div');
+    printContent.id = 'printcontent';
+    printContent.className = 'printcontent';
+    printDiv.append(printContent);
 
     // Create Title
     const printTitle = document.createElement('p');
@@ -24,26 +28,60 @@ function createDiv (titleValue,aboutValue) {
     printTitle.className = 'printTitle';
     console.log(arrTitle)
     printTitle.innerText = titleValue ;
-    printDiv.append(printTitle);
+    printContent.append(printTitle);
 
     // Create About
     const printAbout = document.createElement('p');
     printAbout.id = 'printAbout';
     printAbout.className = 'printAbout';
     printAbout.innerHTML = aboutValue;
-    printDiv.append(printAbout);
+    printContent.append(printAbout);
 
-    
+    //img Div
+    const printImg = document.createElement('div');
+    printImg.id = 'printImg';
+    printDiv.append(printImg);
+
+    // Create Delete Button
+    const img = document.createElement('img');
+    const btn__img = document.createElement('button');
+    btn__img.id = 'btn__img';
+    btn__img.className = 'btn';
+    printImg.append(btn__img);
+    btn__img. addEventListener ('click', () => {
+        for(i=0;i<=arrTitle.length;i++){            
+            printDiv.style.display = 'none';
+            arrTitle.splice(i-1,i);
+        }
+        console.log(arrTitle);
+        // document.getElementById('printContent').innerHTML = arrTitle;
+    })
+    img.id = '';
+    img.className = 'delete__img';
+    img.src = './asset/delete.svg'
+    btn__img.append(img);
 }
 
 
 const section = document.createElement('section')
 container.append(section)
-btn.addEventListener('click', () => {
-    // createDiv();
+btn.addEventListener('click', () => {    
+    // error()
+    
     const title = document.getElementById('title')
-    const about = document.getElementById('about')
-    arrTitle.push({title:title.value,about:about.value})
+    const about = document.getElementById('about')  
+    
+    if(title.value == '' || title.value == null && about.value == '' || about.value == null ){
+        errTitle.style.display = 'block'
+        errAbout.style.display = 'block' 
+
+    }else{
+        errTitle.style.display = 'none'
+        errAbout.style.display = 'none'
+        arrTitle.push({title:title.value,about:about.value})
+    } 
+    // createDiv();
+    
     title.value = ''
     about.value = ''
 
